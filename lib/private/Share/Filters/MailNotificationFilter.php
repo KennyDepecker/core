7d14c8551f6f23107f21ca4580680f3370c5dbec
Matthew Setter <matthew@matthewsetter.com>
2018-01-05 13:05:40 +0100

    Add file and class docblock comments

--- lib/private/Share/Filters/MailNotificationFilter.php
+++ lib/private/Share/Filters/MailNotificationFilter.php
@@ -1,9 +1,38 @@
 <?php
+/**
+ * @author Matthew Setter <matthew@matthewsetter.com>
+ *
+ * @copyright Copyright (c) 2017, ownCloud GmbH
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
 
 namespace OC\Share\Filters;
 
 use Zend\InputFilter\Factory as InputFilterFactory;
 
+/**
+ * Class MailNotificationFilter
+ *
+ * This class provies a programmatic way of filtering data used in
+ * MailNotification::sendLinkShareMail, to ensure that it's not tainted nor
+ * contains any undesired information.
+ *
+ * @package OC\Share
+ * @see https://hackerone.com/reports/297535
+ */
 class MailNotificationFilter {
 
 	/**
